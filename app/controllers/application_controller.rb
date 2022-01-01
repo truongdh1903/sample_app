932994d5c1e685f39adbc081af1e08349cdf7fbd
= <=>
2022-01-01 14:31:27 +0700

    Chapter 8: Basic Login

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,4 +1,5 @@
 class ApplicationController < ActionController::Base
+  include SessionsHelper
   before_action :set_locale
   def set_locale
     locale = params[:locale].to_s.strip.to_sym
--- config/routes.rb
+++ config/routes.rb
@@ -6,6 +6,9 @@ Rails.application.routes.draw do
     get "static_pages/contact"
     get "static_pages/about"
     get "/signup", to: "users#new"
-    resources :users, only: %i(new, create, show)
+    get "/login", to: "sessions#new"
+    post "/login", to: "sessions#create"
+    delete "/logout", to: "sessions#destroy"
+    resources :users, except: %i(:edit, :update, :destroy)
   end
 end
