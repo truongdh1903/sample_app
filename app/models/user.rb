11cf6746278f67230c6c0366deae23110acabf96
= <=>
2022-01-11 13:18:29 +0700

    Chapter 11: Activating The Account

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,5 +1,7 @@
 class User < ApplicationRecord
-  attr_accessor :remember_token
+  attr_accessor :remember_token, :activation_token
+  before_save :downcase_email
+  before_create :create_activation_digest
   validates :name, presence: true
   VALID_EMAIL_REGEX = /\A[\w+\-.]+@[a-z\d\-.]+\.[a-z]+\z/i.freeze
   validates :email, presence: true, length: {maximum: 255},
@@ -30,14 +32,21 @@ class User < ApplicationRecord
   end
 
   # Returns true if the given token matches the digest.
-  def authenticated? remember_token
+  def authenticated? attribute, token
+    digest = send "#{attribute}_digest"
     return false if remember_digest.nil?
 
-    BCrypt::Password.new(remember_digest).is_password?(remember_token)
+    BCrypt::Password.new(digest).is_password? token
   end
 
   # Forgets a user
   def forget
     update_attribute(:remember_digest, nil)
+  end
+
+  # Creates and assigns the activation token and digest.
+  def create_activation_digest
+    self.activation_token = User.new_token
+    self.activation_digest = User.digest activation_token
   end
 end
