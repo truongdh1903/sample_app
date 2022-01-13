51d8920f957b13b9e13688b84782e5c44b11a105
= <=>
2022-01-13 09:28:50 +0700

    Chapter 12: Password Reset

--- config/environments/development.rb
+++ config/environments/development.rb
@@ -69,6 +69,7 @@ Rails.application.configure do
   # config.action_cable.disable_request_forgery_protection = true
 
   config.action_mailer.delivery_method = :smtp
+  config.action_mailer.default_url_options = {host: ENV["host"] }
   config.action_mailer.smtp_settings = {
     address: "smtp.gmail.com",
     port: 587,
